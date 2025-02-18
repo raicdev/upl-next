82463f7210e01d6cb04ad30e124d0053faccccf2
raidesuuu <contact@raic.tech>
2025-02-18 20:46:13 +0900

    fix(chat): if not available, return null

--- packages/firebase/src/server.ts
+++ packages/firebase/src/server.ts
@@ -1,10 +1,9 @@
-import { initializeApp, cert, getApps } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { getDatabaseWithUrl } from 'firebase-admin/database';
-import { getFirestore } from 'firebase-admin/firestore';
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getDatabaseWithUrl } from "firebase-admin/database";
+import { getFirestore } from "firebase-admin/firestore";
 
 if (!getApps()?.length) {
-  
   if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY === undefined) {
     console.error("FIREBASE_SERVICE_ACCOUNT_KEY is not defined in .env file");
   } else {
@@ -12,15 +11,29 @@ if (!getApps()?.length) {
       credential: cert(
         // 環境変数から認証情報を取得
         JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
-      )
+      ),
     });
   }
 }
 
-
-export const notAvailable = !process.env.FIREBASE_SERVICE_ACCOUNT_KEY || process.env.FIREBASE_SERVICE_ACCOUNT_KEY === undefined;
-export const authAdmin = getAuth();
-export const firestoreAdmin = getFirestore();
-export const databaseAdmin = getDatabaseWithUrl(
-  "https://e-mediator-401323-default-rtdb.firebaseio.com"
-);
+export const notAvailable =
+  !process.env.FIREBASE_SERVICE_ACCOUNT_KEY ||
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY === undefined;
+export const authAdmin = () => {
+  if (notAvailable) {
+    return null;
+  }
+  getAuth();
+};
+export const firestoreAdmin = () => {
+  if (notAvailable) {
+    return null;
+  }
+  getFirestore();
+};
+export const databaseAdmin = () => {
+  if (notAvailable) {
+    return null;
+  }
+  getDatabaseWithUrl("https://e-mediator-401323-default-rtdb.firebaseio.com");
+};
